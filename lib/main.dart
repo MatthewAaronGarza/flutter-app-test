5ad3c905791e4c66f60dc6d7fd1f834ae8f29fb5
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-28 14:31:32 -0500

    Create login/register buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+
     return MaterialApp(
       title: 'My First Flutter Project',
       theme: ThemeData(
@@ -15,10 +15,45 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My First Flutter Project'),
+          title: Text('My First Flutter Project'),
         ),
-        body: const Center(
-          child: Text('Hello from Maine!'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Perform login action
+                },
+                child: const Text('Login'),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(200, 50)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
+                    )
+                  )
+                )
+              ),
+              SizedBox(height: 20),
+              OutlinedButton(
+                onPressed: () {
+                  // Perform register action
+                },
+                child: Text('Register'),
+
+                  style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(180, 40)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+
+              ),
+            ],
+          ),
         ),
       ),
     );
