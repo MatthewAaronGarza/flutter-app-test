85d2a62f67d71b6f045901882099799bad586a24
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-28 15:08:43 -0500

    Create firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 
 void main() async {
@@ -8,7 +10,7 @@ void main() async {
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   runApp(MyApp());
 }
 
@@ -66,5 +68,28 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+Future<String?> signUpWithEmailAndPassword(String email, String password) async {
+  try {
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    User? user = userCredential.user;
+    return user?.uid;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      return 'The password provided is too weak.';
+    } else if (e.code == 'email-already-in-use') {
+      return 'The account already exists for that email.';
+    }
+    return e.message;
+  } catch (e) {
+    return 'Error: $e';
   }
 }
